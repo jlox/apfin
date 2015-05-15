b1b8a687a2d4db34600b0ddfe4031552623d1fc9
jlox <altermuse3@gmail.com>
2015-05-15 10:22:36 -0400

    started final project; notes

---
+++ Entry.java
@@ -0,0 +1,17 @@
+public class Entry{
+    String eng, ger;
+    private String def;
+
+    /*
+entries consist of word and definition
+make arrays containing linkedlists - each index holds and entry that is a linkedlist
+
+array (dictionary) containing entries that are comprised of the word(s) and definition(s)
+the entry is made up of linkedLists, where the first node is the word and it points to definition(s)
+
+have two sets of arrays, where one has english pointing to german and the other has german pointing to english
+
+
+2d array with english word at first index, and then german in later indeces (of the internal array)
+    */
+}
