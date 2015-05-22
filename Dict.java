b16847ae7adf7718a6c0351d44b7c68897a55905
timon.luo <tjieluo.capri@gmail.com>
2015-05-22 08:25:02 -0400

    dictionary

---
+++ Dict.java
@@ -0,0 +1,13 @@
+public class Dict{
+    private ArrayList<Entry> entries = new ArrayList<Entry>();
+    
+    public void load() {
+	// loads dictionary
+    }
+
+    public String search(String deutsch) {
+	
+    }
+
+
+}
